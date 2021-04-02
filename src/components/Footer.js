fd452d89c2469c93d60008366f94bba93ccbc867
jonnahmarie <jonnah.marie@gmail.com>
2021-04-02 00:01:07 -0700

    added react Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <p>
+                Employee photos cropped from <a href="https://theoffice.fandom.com/">DunderPedia</a>
+            </p>
+            <p>
+                <em>
+                    The Office (US) &#38; Dunder Mifflin Paper Company are property of NBC and was developed for television by Greg Daniels.
+                </em>
+            </p>
+            <p>
+                This directory was built by <a href="https://jonnahmarie.github.io/">Jonnah Marie Pangilinan</a> on 4/2/2021.
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
